cde8b1faf82c513a55566829a3d2a270f1fc6bed
Marcin Koszykowski <m.koszykowski92@gmail.com>
2019-10-07 16:50:42 +0200

    Added Error

---
+++ src/components/atoms/ErrorText.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import Colors from 'styled/Colors';
+
+const ErrorText = styled.p`
+  font-size: 2rem;
+  color: ${Colors.white};
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-weight: 600;
+`;
+
+export default ErrorText;
---
+++ src/components/molecules/Error.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import Colors from 'styled/Colors';
+import { error as errorValue } from 'data/Value';
+import ErrorText from 'components/atoms/ErrorText';
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 140px;
+  left: 50%;
+  background-color: ${Colors.red};
+  text-align: center;
+  justify-content: center;
+  border-radius: 20px;
+  padding: 25px 100px;
+  transform: ${({ error }) => (error ? `translate(-50%, 0)` : `translate(-50%, -300%)`)};
+  transition: transform 0.5s;
+`;
+
+const Error = ({ error }) => (
+  <Wrapper error={error}>
+    <ErrorText>{errorValue}</ErrorText>
+  </Wrapper>
+);
+
+export default Error;
