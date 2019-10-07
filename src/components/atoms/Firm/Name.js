579c7be03d359ccc2ccedc2876711f724d590661
Marcin Koszykowski <m.koszykowski92@gmail.com>
2019-10-07 07:57:58 +0200

    Added atoms Firm

---
+++ src/components/atoms/Firm/Name.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Name = styled.p`
+  margin: 0;
+  height: 23px;
+  font-size: 1.2rem;
+  font-weight: 500;
+`;
+
+export default Name;
---
+++ src/components/atoms/Firm/SecondName.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const SecondName = styled.p`
+  height: 18px;
+  margin-bottom: 3px;
+  font-size: 0.9rem;
+`;
+
+export default SecondName;
